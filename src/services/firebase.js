8690b3e1354c843fd4dc86ef9b0b8db0f4b3d69a
Julio Meza <juliomeza@gmail.com>
2024-08-06 09:36:53 -0400

    api key

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,28 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from 'firebase/firestore';
 
-// Configuration for local development
-const localFirebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || (process.env.FIREBASE_CONFIG && JSON.parse(process.env.FIREBASE_CONFIG).react.app.firebase_api_key),
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || (process.env.FIREBASE_CONFIG && JSON.parse(process.env.FIREBASE_CONFIG).react.app.firebase_auth_domain),
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || (process.env.FIREBASE_CONFIG && JSON.parse(process.env.FIREBASE_CONFIG).react.app.firebase_project_id),
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || (process.env.FIREBASE_CONFIG && JSON.parse(process.env.FIREBASE_CONFIG).react.app.firebase_storage_bucket),
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || (process.env.FIREBASE_CONFIG && JSON.parse(process.env.FIREBASE_CONFIG).react.app.firebase_messaging_sender_id),
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || (process.env.FIREBASE_CONFIG && JSON.parse(process.env.FIREBASE_CONFIG).react.app.firebase_app_id)
 };
 
-// Use local config for development, empty object for production
-const firebaseConfig = process.env.NODE_ENV === 'production' ? {} : localFirebaseConfig;
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
-
-// Inicializar Firestore con persistencia
 const db = initializeFirestore(app, {
   localCache: persistentLocalCache({
     tabManager: persistentMultipleTabManager()
