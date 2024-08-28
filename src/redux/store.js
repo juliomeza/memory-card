ff35101efe404d67440851e08e2fbd2305d8c4c7
Julio Meza <juliomeza@gmail.com>
2024-08-28 10:29:28 -0400

    MiddleWare

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import gameReducer from './slices/gameSlice';
+import { loggerMiddleware } from './middleware/loggerMiddleware';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     game: gameReducer,
   },
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware().concat(loggerMiddleware),
 });
